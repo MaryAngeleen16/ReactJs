841d5fad32e1d647b410c18d872785e650da22b5
MaryAngeleen16 <angelpagalan38@gmail.com>
2023-09-26 08:41:08 +0800

    second commit

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -3,80 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 use App\Models\Post;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $posts = Post::all();
-        return response()->json(['posts' => $posts]);
+        return response()->json(["posts" => $posts], 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        $users = User::all();
+        return response()->json(["users" => $users], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $post = new Post();
-        $post->title = $request->title;
-        $post->slug = $request->slug;
-        $post->content = $request->content;
-        $post->user_id = '1';
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->slug = $request->input('slug');
+        $post->user_id = $request->input('user');
         $post->save();
 
-        return response()->json(['status' => 'post saved', 'code' => 200]);
+        return response()->json(["post" => $post, "message" => "successfully created"], 200);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $post = Post::find($id);
+        return response()->json(["post" => $post], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        $post = Post::find();
-        return;
+        $post = Post::find($id);
+        return response()->json(["post" => $post], 200);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         $post = Post::find($id);
-        $post->title=$request->title;
-        $post->slug=$request->slug;
-        $post->content = $request->content;
-        $post->user_id='1';
-        $post->save();
-        return response()->json(['status'=>'post saved', 'code'=>201]);
+        $post->update($request->all());
 
+        return response()->json(["post" => $post, "message" => "successfully updated"], 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         Post::destroy($id);
-        return response()->json(['status'=>'post deleted', 'code'=>200]);
+        return response()->json(["message" => "successfully deleted"], 200);
     }
 }
